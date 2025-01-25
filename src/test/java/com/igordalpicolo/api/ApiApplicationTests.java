7e9ef3efabb93de813ef906626b2698b515e5264
Igor Dal Picolo <igordalpicolo11@hotmail.com>
2025-01-25 18:51:59 -0300

    Project created

---
+++ src/test/java/com/igordalpicolo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igordalpicolo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
